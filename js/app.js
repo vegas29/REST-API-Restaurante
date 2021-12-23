533a6fbe2c4ebfd46c6e74b69667232d0911b8e0
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2021-12-23 07:13:30 -0500

    Scripting para estilizar los resultados de la api

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ let cliente = {
     platillos: []
 };
 
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres'
+}
+
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', guardarCliente);
 
@@ -31,4 +37,65 @@ function guardarCliente(){
     const modalFormulario = document.querySelector('#formulario');
     const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
     modalBootstrap.hide();
+
+    //Mostrar las secciones
+    mostrarSecciones();
+
+    //Obtener platillos de la API de JSON SERVER
+    obtenerPlatillos();
+    
+}
+
+function mostrarSecciones(){
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
+
+}
+
+function obtenerPlatillos(){
+    const url = "http://localhost:4000/platillos";
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => mostrarPlatillos(resultado))
+        .catch(error => console.log(error))
+}
+
+function mostrarPlatillos(platillos){
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platillos.forEach(platillo => {
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+
+        const nombre = document.createElement('DIV');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        const precio = document.createElement('DIV');
+        precio.classList.add('col-md-3', 'fw-bold');
+        precio.textContent = `$${platillo.precio}`;
+
+        const categoria = document.createElement('DIV');
+        categoria.classList.add('col-md-3');
+        categoria.textContent = categorias[platillo.categoria];
+
+        const input = document.createElement('INPUT');
+        input.type = 'number';
+        input.min = 0;
+        input.value = 0;
+        input.id = `producto-${platillo.id}`;
+        input.classList.add('form-control');
+
+        const agregar = document.createElement('DIV');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(input);
+
+        row.appendChild(nombre);
+        row.appendChild(precio);
+        row.appendChild(categoria);
+        row.appendChild(agregar);
+
+        contenido.appendChild(row);
+    })
 }
